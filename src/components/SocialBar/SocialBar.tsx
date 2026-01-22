a974fa5241f0330c0f23638d217fd6df63115a36
mherrada <mherrada@falabella.cl>
2026-01-22 12:37:47 -0300

    feat: Remove social networks rel

--- src/components/SocialBar/SocialBar.tsx
+++ src/components/SocialBar/SocialBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./SocialBar.module.css";
 import githubIcon from "../../assets/icons/github.svg";
 //import linkedinIcon from "../../assets/icons/linkedin.svg";
-import xIcon from "../../assets/icons/x.svg";
+//import xIcon from "../../assets/icons/x.svg";
 import coffeeIcon from "../../assets/icons/coffee.ico";
 
 
@@ -13,11 +13,7 @@ export const SocialBar: React.FC = () => {
         <img src={githubIcon} alt="GitHub" className={styles.icon} />
       </a>
 
-      <a href="https://x.com/YOUR_USERNAME" target="_blank" rel="noopener noreferrer">
-        <img src={xIcon} alt="X" className={styles.icon} />
-      </a>
-
-      <a href="buymeacoffee.com/reenignedren" target="_blank" rel="noopener noreferrer">
+      <a href="https://buymeacoffee.com/reenignedren" target="_blank" rel="noopener noreferrer">
         <img src={coffeeIcon} alt="Buy Me a Coffee" className={styles.icon} />
       </a>
     </div>
